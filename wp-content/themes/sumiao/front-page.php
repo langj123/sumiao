5eea3487045d058e0f77a168c26dcf50ceeb6cea
Jacob Lang <langj123@gmail.com>
2016-12-22 17:40:50 -0500

    front page fix

--- wp-content/themes/sumiao/front-page.php
+++ wp-content/themes/sumiao/front-page.php
@@ -16,6 +16,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<h1>hellow</h1>
 		<?php get_template_part( 'template-parts/content', 'page' ); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
